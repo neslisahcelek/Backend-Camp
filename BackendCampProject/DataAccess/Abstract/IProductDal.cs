dba6261ceb0880cb73d2330c0cfe328ed581fdcf
Neslisah <neslisah.celek@outlook.com>
2023-04-06 15:36:23 +0300

    .

--- BackendCampProject/DataAccess/Abstract/IProductDal.cs
+++ BackendCampProject/DataAccess/Abstract/IProductDal.cs
@@ -7,12 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal
+    public interface IProductDal: IEntityRepository
     {
-        List<Product> GetAll();
-        void Add(Product product);
-        void Update(Product product);   
-        void Delete(Product product);   
-        List<Product> GetAllByCategory(int categoryId);
+        
     }
 }
